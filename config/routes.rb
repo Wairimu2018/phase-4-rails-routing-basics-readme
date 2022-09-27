9728f0484367959fc79a40970013eb4cbc9a9980
Wairimu2018 <wairimukuria.wk@gmail.com>
2022-09-27 14:08:00 +0300

    basic routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+#get - HHTP verb
+#cheese - Path in the url that the route will be mapped to
+#cheese#index - Controller action
+
+  get 'cheeses', to: 'cheeses#index'
 end
